ab847e24d22033e4a4903f1f56017ef8bdf760b7
Sajjad Hossain <sajjad.hossain@stud.uni-bamberg.de>
2025-05-01 23:16:27 +0200

    #6 - Change input reading funtionality

--- week_2_tasks/config.py
+++ week_2_tasks/config.py
@@ -1,7 +1,15 @@
+from tkinter import TOP
+
+
 CHUNK_SIZE = 200
 EMBEDDING_MODEL = 'all-MiniLM-L6-v2'
-INPUT_DIRECTORY = 'documents'
+INPUT_FILE_PATH = 'data/sample_texts.csv'
 COSINE_VIS_TITLE = 'Cosine Similarity Between Text Samples'
 TSNE_VIS_TITLE = 'Sentence Embeddings Visualized with t-SNE'
 PCA_VIS_TITLE = 'Sentence Embeddings Visualized with PCA'
-LLM_MODEL = 'google/flan-t5-base'
+# this model worked but the other one has bigger parameters
+# LLM_MODEL = 'google/flan-t5-small'
+# this model is not working in Mac M1 and giving segmentation fault error
+# LLM_MODEL = 'google/flan-t5-base'
+LLM_MODEL = 'declare-lab/flan-alpaca-base'
+TOP_K = 5
