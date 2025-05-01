aa0e19d64b3f3b9d220aa6b73e305b2a780adc9a
Sajjad Hossain <sajjad.hossain@stud.uni-bamberg.de>
2025-05-01 23:19:11 +0200

    #6 - Run predefined questiion and generate some matrices

--- week_2_tasks/main.py
+++ week_2_tasks/main.py
@@ -2,131 +2,119 @@
 Main module for text embedding and visualization.
 This script processes text files, generates embeddings, and visualizes them in different ways.
 """
-import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 from sklearn.metrics.pairwise import cosine_similarity
-import faiss
-import numpy as np
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-# import torch
 
-from text_embedding.load_model import load_transformer_model
-from read_documents.read_txt_files import get_text_chunks_from_directory
-from text_embedding.generate_embedding import generate_embedding
+from read_documents.document_reader import read_csv, chunk_text
 from analysis.visualize_embedding import (
     visualize_using_pca,
     visualize_using_tsne,
     visualize_cosine_similarity
 )
+from utils.llm import invoke_llm
+from utils.prompts import generate_answering_prompt, generate_answer_evaluation_prompt
+from utils.retriever import store_embeddings, retrieve_similar_embeddings, get_retrieved_chunks, generate_document_embeddings
+from utils.evaluation import rank_chunks_by_ratings, run_with_timer
 from config import (
     CHUNK_SIZE,
-    EMBEDDING_MODEL,
-    INPUT_DIRECTORY,
     COSINE_VIS_TITLE,
     TSNE_VIS_TITLE,
     PCA_VIS_TITLE,
-    LLM_MODEL
+    INPUT_FILE_PATH,
+    TOP_K
 )
 
-
-def process_documents():
-    """Process documents and return their contents and names."""
-    txt_files_directory = INPUT_DIRECTORY
-    return get_text_chunks_from_directory(txt_files_directory, chunk_size=CHUNK_SIZE)
-
-
-def generate_document_embeddings(file_contents):
-    """Generate embeddings from file contents using the configured model."""
-    model = load_transformer_model(EMBEDDING_MODEL)
-    embeddings = generate_embedding(file_contents, model)
-    
-    del model
-    
-    return embeddings
-
-
-def visualize_embeddings(embeddings, sample_names):
+def visualize_topic_embeddings(topic_contents, labels):
     """Visualize embeddings using different techniques."""
+    embeddings = generate_document_embeddings(topic_contents)
     # Calculate and visualize cosine similarity
     similarity_matrix = cosine_similarity(embeddings)
-    visualize_cosine_similarity(similarity_matrix, sample_names, graph_title=COSINE_VIS_TITLE)
+    visualize_cosine_similarity(similarity_matrix, labels, graph_title=COSINE_VIS_TITLE)
     
     # Visualize using dimensionality reduction techniques
-    visualize_using_pca(embeddings, sample_names, graph_title=PCA_VIS_TITLE)
-    visualize_using_tsne(embeddings, sample_names, graph_title=TSNE_VIS_TITLE)
-    
-def store_embeddings(embeddings):
-    db = faiss.IndexFlatL2(embeddings[0].shape[0])
-    db.add(np.array(embeddings))
-    
-    return db
+    visualize_using_pca(embeddings, labels, graph_title=PCA_VIS_TITLE)
+    visualize_using_tsne(embeddings, labels, graph_title=TSNE_VIS_TITLE)  
 
-def retrieve_similar_embeddings(db, query, k=5):
-    """Retrieve the top k most similar embeddings from the database."""
-    query_embedding = generate_document_embeddings([query])
-    distances, indices = db.search(np.array(query_embedding), k=k)
-    return distances, indices
-
-def get_retrieved_chunks(indices, file_contents):
-    """Retrieve the chunks corresponding to the indices."""
-    retrieved_chunks = []
-    for i in indices[0]:
-        retrieved_chunks.append(file_contents[i])
-    return retrieved_chunks
-
-def generate_prompt(retrieved_chunks, query):
-    context  = "\n\n".join(retrieved_chunks)
+def get_qna_tuples(df):
+    question_columns = ['q1', 'q2', 'q3', 'q4', 'q5']
+    answer_columns = ['a1', 'a2', 'a3', 'a4', 'a5']
     
-    prompt = f"""You are a helpful assistant. Use the following context to answer the question.
-
-    Context:
-    {context}
-
-    Question:
-    {query}
-
-    Answer:"""
-    
-    return prompt
-    
-def generate_answer(prompt):
-    tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL)
-    model = AutoModelForSeq2SeqLM.from_pretrained(LLM_MODEL)
-    inputs = tokenizer("Can yo answer this?", return_tensors="pt", truncation=True)
-    
-    with torch.no_grad():
-        outputs = model.generate(**inputs, max_new_tokens=100)
+    qna_tuples = []
+    for _, row in df.iterrows():
+        row_qna = []
+        for q_col, a_col in zip(question_columns, answer_columns):
+            row_qna.append((row[q_col], row[a_col]))
+        qna_tuples.append(row_qna)
         
-    # answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    answer = 'I do not know the answer to that question.'
+    return qna_tuples
+
+def run_and_evaluate_answer(db, file_chunks, qna_tuples):
+    """Run the LLM and evaluate the answers."""
+    execution_times = []
+    chunk_ratings = []
+    evaluation_Scores = []
     
-    del tokenizer
-    del model
-    torch.cuda.empty_cache()
+    for topic_wise_qna in qna_tuples:
+        chunk_ratings = {}
+        topic_execution_times = []
+        answer_scores = []
+        
+        
+        for q, a in topic_wise_qna:
+            print(f"Question: {q}")
+            distances, indices = retrieve_similar_embeddings(db, q, k=TOP_K)
+            retrieved_chunks = get_retrieved_chunks(indices, file_chunks)
+            answering_prompt = generate_answering_prompt(retrieved_chunks, q)
+            
+            chunk_ratings = rank_chunks_by_ratings(retrieved_chunks, chunk_ratings)
+            
+            execution_time, answer = run_with_timer(invoke_llm, answering_prompt)
+            topic_execution_times.append(execution_time)
+            # print(f"LLM execution time: {execution_time:.2f} seconds")
+            # print(f"Question: {q}\n")
+            # print(f"AI Answer: {answer}\n")
+            # print(f"Expected Answer: {a}\n")
+            feedback_prompt = generate_answer_evaluation_prompt(q, answer, a)
+            accuracy_score = invoke_llm(feedback_prompt)
+            answer_scores.append(accuracy_score)
+            # print(f"Accuracy Score: {accuracy_score}\n")
+            # print(f"Explaination: {explaination}\n")
+            # print("-" * 50)
     
-    return answer
+        chunk_ratings.append(list(chunk_ratings.values()))
+        execution_times.append(topic_execution_times)
+        evaluation_Scores.append(answer_scores)
+        
+    return execution_times, chunk_ratings, evaluation_Scores
 
 def main():
     """Main function to orchestrate the document processing and visualization workflow."""
     # Process documents
-    file_contents, sample_names = process_documents()
+    df = read_csv(INPUT_FILE_PATH)
+    file_contents = df['text']
+    file_names = df['name']
+    file_chunks = [chunk for content in file_contents for chunk in chunk_text(content, CHUNK_SIZE)]
     
-    # Generate embeddings
-    embeddings = generate_document_embeddings(file_contents)
+    # Visualize results
+    visualize_topic_embeddings(file_contents, file_names)
+ 
     
-    # # Visualize results
-    visualize_embeddings(embeddings, sample_names)
+    # Store embeddings in a database, this needs to be a single dimensional array
+    embeddings = generate_document_embeddings(file_chunks)
+    db = store_embeddings(embeddings)
     
-    # db = store_embeddings(embeddings)
-    # query = 'What are some common reasons university students in Germany don’t always eat healthily?' 
-    # distances, indices = retrieve_similar_embeddings(db, query, 5)
-    # retrieved_chunks = get_retrieved_chunks(indices, file_contents)
-    # prompt = generate_prompt(retrieved_chunks, query)
+    qna_tuples = get_qna_tuples(df)
+    
+    total_execution_time, (execution_times, chunk_ratings, evaluation_Scores) = run_with_timer(run_and_evaluate_answer, db, file_chunks, qna_tuples)
     
     
-    # answer = generate_answer(prompt)
-    # print(f"Answer: {answer}")
+    print(f"Total execution time: {total_execution_time:.2f} seconds")    
+    print("Execution times for each topic:", execution_times)
+    print("Chunk ratings for each topic:", chunk_ratings)
+    print("Evaluation scores for each topic:", evaluation_Scores)
 
 
 if __name__ == "__main__":
