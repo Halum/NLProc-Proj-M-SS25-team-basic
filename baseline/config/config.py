18cd6909040797433ea3c0219648ccd3c99213af
Sajjad Hossain <sajjad.hossain@stud.uni-bamberg.de>
2025-05-20 20:38:55 +0200

    #30  - Match test input file name

--- baseline/config/config.py
+++ baseline/config/config.py
@@ -8,7 +8,7 @@ used throughout the application.
 DOCUMENT_FOLDER_PATH='baseline/data/raw/'
 INSIGHT_FOLDER_PATH='baseline/data/insight/'
 DB_INDEX_PATH='baseline/data/db/'
-TEST_QUESTIONS_PATH='baseline/data/tests/test_questions.json'
+TEST_QUESTIONS_PATH='baseline/data/tests/test_input.json'
 EMBEDDING_MODEL = 'all-MiniLM-L6-v2'
 LLM_MODEL = 'declare-lab/flan-alpaca-base'
 GROUP_ID = 'Team Basic'
